408d7cd2f6988ff036da93b214c7c659c7583f86
Ravi Nariya <ravinariya@Ravis-MacBook-Air.local>
2024-12-03 08:33:53 +0530

    initial setup

---
+++ src/app/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dx-header',
+  templateUrl: './header.component.html',
+  styleUrl: './app.component.scss',
+  standalone: true,
+})
+export class HeaderComponent {}
